7f22b8ae9b7a2055cfc2b86b176278412e203070
bongu kishor <bongukishor320@gmail.com>
2025-08-13 18:15:54 +0530

    initial commit

---
+++ OD-MANAGMENT/src/main/java/odproject/org/OdManagement/OdManagementApplication.java
@@ -0,0 +1,13 @@
+package odproject.org.OdManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdManagementApplication.class, args);
+	}
+
+}
