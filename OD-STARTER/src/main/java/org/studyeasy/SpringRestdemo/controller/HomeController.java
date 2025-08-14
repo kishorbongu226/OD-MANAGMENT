cf3ab3e8f1549e45f487ac5c3a0e1b545ef54906
kishorbongu226 <bongukishor320@gmail.com>
2025-08-14 23:08:54 +0530

    Token generation successful

--- OD-STARTER/src/main/java/org/studyeasy/SpringRestdemo/controller/HomeController.java
+++ OD-STARTER/src/main/java/org/studyeasy/SpringRestdemo/controller/HomeController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public String demo(){
-        return "Hello World";
-    }
+  
 
     
 }
